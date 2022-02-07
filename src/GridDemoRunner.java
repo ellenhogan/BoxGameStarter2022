a7e17dc3fe5e9a8e3f2afb40fe81fe63c83eb815
averybrazelton <avery.brazelton@kinkaid.org>
2022-02-07 14:47:11 -0600

    check

--- src/GridDemoRunner.java
+++ src/GridDemoRunner.java
@@ -5,8 +5,9 @@ public class GridDemoRunner
 	{
 
 		// by default, we're using the StartHereFrame so you can get used to the Cell class;
-		// switch the commenting between these lines when you are ready for the full program.
+		// switch the comme
 
+		// hello
 		StartHereFrame practiceApp = new StartHereFrame();
 //		GridDemoFrame app = new GridDemoFrame();
 
