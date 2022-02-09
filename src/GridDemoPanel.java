e4ba957024045527b5cf774fccd5310d08a8bf41
averybrazelton <avery.brazelton@kinkaid.org>
2022-02-09 14:15:55 -0600

    Board fills with same colors

--- src/GridDemoPanel.java
+++ src/GridDemoPanel.java
@@ -20,10 +20,12 @@ public class GridDemoPanel extends JPanel implements MouseListener, KeyListener
 	{
 		super();
 		resetCells();
+
+
 		//theGrid[2][2].setMarker("A");
-		theGrid[2][2].setDisplayMarker(true);
+		//theGrid[2][2].setDisplayMarker(true);
 	//	theGrid[3][3].setIsLive(false);
-		setBackground(Color.BLACK);
+		//setBackground(Color.BLACK);
 		addMouseListener(this);
 		//parent.addKeyListener(this); // activate this if you wish to listen to the keyboard. 
 		myParent = parent;
@@ -35,9 +37,35 @@ public class GridDemoPanel extends JPanel implements MouseListener, KeyListener
 	public void resetCells()
 	{
 		theGrid = new Cell[NUM_ROWS][NUM_COLS];
-		for (int r =0; r<NUM_ROWS; r++)
-			for (int c=0; c<NUM_COLS; c++)
-				theGrid[r][c] = new Cell(r,c);
+		//for (int r =0; r<NUM_ROWS; r++)
+		//for (int c=0; c<NUM_COLS; c++)
+		theGrid[0][0] = new Cell(4,0,0,"",false);
+		theGrid[0][1] = new Cell(3,0,1,"",false);
+		theGrid[0][2] = new Cell(5,0,2,"",false);
+		theGrid[0][3] = new Cell(2,0,3,"",false);
+		theGrid[0][4] = new Cell(5,0,4,"",false);
+		theGrid[1][0] = new Cell(5,1,0,"",false);
+		theGrid[1][1] = new Cell(2,1,1,"",false);
+		theGrid[1][2] = new Cell(5,1,2,"",false);
+		theGrid[1][3] = new Cell(4,1,3,"",false);
+		theGrid[1][4] = new Cell(3,1,4,"",false);
+		theGrid[2][0] = new Cell(3,2,0,"",false);
+		theGrid[2][1] = new Cell(5,2,1,"",false);
+		theGrid[2][2] = new Cell(4,2,2,"",false);
+		theGrid[2][3] = new Cell(5,2,3,"",false);
+		theGrid[2][4] = new Cell(2,2,4,"",false);
+		theGrid[3][0] = new Cell(2,3,0,"",false);
+		theGrid[3][1] = new Cell(1,3,1,"",false);
+		theGrid[3][2] = new Cell(3,3,2,"",false);
+		theGrid[3][3] = new Cell(5,3,3,"",false);
+		theGrid[3][4] = new Cell(5,3,4,"",false);
+		theGrid[4][0] = new Cell(5,4,0,"",false);
+		theGrid[4][1] = new Cell(5,4,1,"",false);
+		theGrid[4][2] = new Cell(2,4,2,"",false);
+		theGrid[4][3] = new Cell(3,4,3,"",false);
+		theGrid[4][4] = new Cell(0,4,4,"",false);
+
+
 		score = 0;
 	}
 	
